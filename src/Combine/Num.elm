c0c6ecbf061a1e3c96117b90fcaddf949e159560
André Dietrich <andredietrich@web.de>
2024-10-20 23:53:27 +0200

    improve(Num): little faster functions

--- src/Combine/Num.elm
+++ src/Combine/Num.elm
@@ -17,6 +17,13 @@ import String
 
 {-| Parse a numeric sign, returning `1` for positive numbers and `-1`
 for negative numbers.
+
+    parse sign "+" == Ok 1
+
+    parse sign "-" == Ok -1
+
+    parse sign "a" == Err [ "expected a sign" ]
+
 -}
 sign : Parser s Int
 sign =
@@ -28,33 +35,49 @@ sign =
 
 
 {-| Parse a digit.
+
+    parse digit "1" == Ok 1
+
+    parse digit "a" == Err [ "expected a digit" ]
+
 -}
 digit : Parser s Int
 digit =
-    let
-        toDigit c =
-            Char.toCode c - Char.toCode '0'
-    in
-    map toDigit Combine.Char.digit |> onerror "expected a digit"
+    Combine.Char.digit
+        -- 48 is the ASCII code for '0'
+        |> map (\c -> Char.toCode c - 48)
+        |> onerror "expected a digit"
 
 
 {-| Parse an integer.
+
+    parse int "123" == Ok 123
+
+    parse int "-123" == Ok -123
+
+    parse int "abc" == Err [ "expected an int" ]
+
 -}
 int : Parser s Int
 int =
     regex "-?(?:0|[1-9]\\d*)"
-        |> map String.toInt
-        |> andThen unwrap
+        |> andThen (String.toInt >> unwrap)
         |> onerror "expected an int"
 
 
 {-| Parse a float.
+
+    parse float "123.456" == Ok 123.456
+
+    parse float "-123.456" == Ok -123.456
+
+    parse float "abc" == Err [ "expected a float" ]
+
 -}
 float : Parser s Float
 float =
     regex "-?(?:0|[1-9]\\d*)\\.\\d+"
-        |> map String.toFloat
-        |> andThen unwrap
+        |> andThen (String.toFloat >> unwrap)
         |> onerror "expected a float"
 
 
